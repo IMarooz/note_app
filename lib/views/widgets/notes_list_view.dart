1f49cd8b1026804ad7f722be7d4809ac335dda5e
Marooz <marwanoffical@gmail.com>
2025-11-30 11:24:11 +0200

    create note edit view

--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:note_app_tharwat/views/edit_note_view.dart';
 import 'package:note_app_tharwat/views/widgets/custom_note_item.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
-  
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
-      child: ListView.builder(
-        padding: const EdgeInsets.all(0),
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(vertical: 4.0),
-            child: NoteItem(),
-          );
-        },
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const EditNoteView()),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: ListView.builder(
+          padding: const EdgeInsets.all(0),
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(vertical: 4.0),
+              child: NoteItem(),
+            );
+          },
+        ),
       ),
     );
   }
