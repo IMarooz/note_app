0d4cb97303651cc4da5a3312b1579a02c1d0c0ca
Marooz <marwanoffical@gmail.com>
2025-12-01 16:49:48 +0200

    Refactor MultiBloc Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app_tharwat/constants.dart';
-import 'package:note_app_tharwat/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app_tharwat/models/note_model.dart';
 import 'package:note_app_tharwat/simple_bloc_observer.dart';
 import 'package:note_app_tharwat/views/notes_view.dart';
@@ -11,8 +10,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
-  await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
+  await Hive.openBox<NoteModel>(kNotesBox);
   runApp(const NoteApp());
 }
 
@@ -21,17 +20,14 @@ class NoteApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [BlocProvider(create: (context) => AddNoteCubit())],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Note App',
-        home: NotesView(),
-        theme: ThemeData(
-          brightness: Brightness.dark,
-          scaffoldBackgroundColor: Colors.grey[900],
-          fontFamily: 'Poppins',
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Note App',
+      home: NotesView(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.grey[900],
+        fontFamily: 'Poppins',
       ),
     );
   }
--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app_tharwat/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:note_app_tharwat/models/note_model.dart';
 import 'package:note_app_tharwat/views/widgets/custom_button.dart';
 import 'package:note_app_tharwat/views/widgets/custom_text_field.dart';
 
@@ -41,6 +44,13 @@ class _AddNoteFormState extends State<AddNoteForm> {
             onTap: () {
               if (_formKey.currentState!.validate()) {
                 _formKey.currentState!.save();
+                var noteModel = NoteModel(
+                  title: title!,
+                  subTitle: body!,
+                  date: DateTime.now().toString(),
+                  color: Colors.blue.toARGB32(),
+                );
+                BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
               } else {
                 autovalidateMode = AutovalidateMode.always;
                 setState(() {});
